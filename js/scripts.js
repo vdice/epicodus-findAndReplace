8a768ae660b5ceb60a3f5ece9eb8472a3e3a17ea
Vaughn Dice <donvice@gmail.com>
2015-09-11 10:00:45 -0700

    add DOM logic to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,18 @@ var findAndReplace = function(inputString, searchKey, replacement) {
 String.prototype.isEmpty = function() {
   return (this.length === 0 || !this.trim());
 };
+
+$(document).ready(function() {
+  $('form').submit(function(event) {
+    var originalString = $('input#original-string').val();
+    var searchString = $('input#search-string').val();
+    var replacementString = $('input#replacement-string').val();
+
+    var newString = findAndReplace(originalString, searchString, replacementString);
+
+    $('#new-string').text(newString);
+    $('#result').fadeIn();
+
+    event.preventDefault();
+  });
+});
