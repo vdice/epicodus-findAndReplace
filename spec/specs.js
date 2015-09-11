2fbbbf8148c10aa1f7f1717ea924c2f2e6b1908c
Vaughn Dice <donvice@gmail.com>
2015-09-11 09:36:49 -0700

    add specs around multi-word replacement and nested use

--- spec/specs.js
+++ spec/specs.js
@@ -3,6 +3,14 @@ describe('findAndReplace', function() {
   var replacement = 'BMW'
   var inputString = 'Do I sound like a ' + searchKey + '?'
 
+  it('returns empty string if input string is empty', function() {
+    expect(findAndReplace('', searchKey, replacement)).to.equal('');
+  });
+
+  it('handles blank/whitespace-only strings', function() {
+    expect(findAndReplace(inputString, '      ', replacement)).to.equal(inputString);
+  });
+
   it('returns input string if no search string is provided', function() {
     expect(findAndReplace(inputString, '', replacement)).to.equal(inputString);
   });
@@ -17,5 +25,13 @@ describe('findAndReplace', function() {
 
   it('returns modified input string when search string and replacement string differ', function() {
     expect(findAndReplace(inputString, searchKey, replacement)).to.equal('Do I sound like a ' + replacement + '?');
+  });
+
+  it('returns modified input string with multi-word search/replacement strings', function() {
+    expect(findAndReplace(inputString, 'Do I', 'You really')).to.equal('You really sound like a ' + searchKey + '?');
+  });
+
+  it('can be used in a nested fashion', function() {
+    expect(findAndReplace(findAndReplace(inputString, 'Do I', 'You really'), '?', '!')).to.equal('You really sound like a ' + searchKey + '!');
   });
 });
