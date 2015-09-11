7121764fdffc94f5f9e61f4403dd3a27a1a4e22b
Vaughn Dice <donvice@gmail.com>
2015-09-11 10:33:10 -0700

    change variable names in specs.js

--- spec/specs.js
+++ spec/specs.js
@@ -1,39 +1,39 @@
 describe('findAndReplace', function() {
-  var searchKey = 'Ducati'
-  var replacement = 'BMW'
-  var inputString = 'Do I sound like a ' + searchKey + '?'
+  var searchString = 'Ducati'
+  var replacementString = 'BMW'
+  var originalString = 'Do I sound like a ' + searchString + '?'
 
   it('returns empty string if input string is empty', function() {
-    expect(findAndReplace('', searchKey, replacement)).to.equal('');
+    expect(findAndReplace('', searchString, replacementString)).to.equal('');
   });
 
   it('handles blank/whitespace-only strings', function() {
-    expect(findAndReplace(inputString, '      ', replacement)).to.equal(inputString);
+    expect(findAndReplace(originalString, '      ', replacementString)).to.equal(originalString);
   });
 
-  it('returns input string if no search string is provided', function() {
-    expect(findAndReplace(inputString, '', replacement)).to.equal(inputString);
+  it('returns original string if no search string is provided', function() {
+    expect(findAndReplace(originalString, '', replacementString)).to.equal(originalString);
   });
 
-  it('returns input string if no replacement string is provided', function() {
-    expect(findAndReplace(inputString, searchKey, '')).to.equal(inputString);
+  it('returns original string if no replacement string is provided', function() {
+    expect(findAndReplace(originalString, searchString, '')).to.equal(originalString);
   });
 
-  it('returns input string if search string is identical to the replacement string', function() {
-    expect(findAndReplace(inputString, searchKey, searchKey)).to.equal(inputString);
+  it('returns original string if search string is identical to the replacement string', function() {
+    expect(findAndReplace(originalString, searchString, searchString)).to.equal(originalString);
   });
 
-  it('returns modified input string when search string and replacement string differ', function() {
-    expect(findAndReplace(inputString, searchKey, replacement)).to.equal('Do I sound like a ' + replacement + '?');
+  it('returns modified original string when search string and replacement string differ', function() {
+    expect(findAndReplace(originalString, searchString, replacementString)).to.equal('Do I sound like a ' + replacementString + '?');
   });
 
-  it('returns modified input string with multi-word search/replacement strings', function() {
-    expect(findAndReplace(inputString, 'Do I', 'You really')).to.equal('You really sound like a ' + searchKey + '?');
+  it('returns modified original string with multi-word search/replacement strings', function() {
+    expect(findAndReplace(originalString, 'Do I', 'You really')).to.equal('You really sound like a ' + searchString + '?');
   });
 
   it('replaces multiple occurences of search string with replacement string', function() {
     expect(findAndReplace('wowowow', 'w', 'z')).to.equal('zozozoz');
-    expect(findAndReplace(inputString, 'D', 'd')).to.equal('do I sound like a ducati?');
+    expect(findAndReplace(originalString, 'D', 'd')).to.equal('do I sound like a ducati?');
   });
 });
 
